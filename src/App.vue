bbd037b6c4b1f52dae3483c52e974e375db16711
khuluqie <andikakhuluqi@student.uir.ac.id>
2025-05-02 15:19:37 +0700

    filter Tugas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const tugas = ref([])
 const tugasBaru = ref('')
+const filter = ref('semua')
 
 const tambahTugas = () => {
   tugas.value.push({
@@ -23,14 +24,28 @@ const hapusTugas = (t) => {
   tugas.value = tugas.value.filter(tugas => tugas.id !== t.id)
 }
 
+const filterTugas = computed(() => {
+  if (filter.value === 'selesai') {
+    return tugas.value.filter(t => t.selesai)
+  } else if (filter.value === 'belum selesai') {
+    return tugas.value.filter(t => !t.selesai)
+  } else {
+    return tugas.value
+  }
+})
+
 </script>
 
 <template>
   <input type="text" v-model="tugasBaru" @keyup.enter="tambahTugas">
   <button @click="tambahTugas">Tambah Tugas</button>
-
+  <div>
+    <button @click="filter = 'semua'">Semua</button>
+    <button @click="filter = 'selesai'">Selesai</button>
+    <button @click="filter = 'belum selesai'">Belum Selesai</button>
+  </div>
   <ul>
-    <li v-for="t in tugas" :key="tugas.id">
+    <li v-for="t in filterTugas" :key="tugas.id">
       <input type="checkbox" v-model="t.selesai" @change="toggle(t)" :checked="t.selesai">
       {{ t.nama }}
       <button @click="hapusTugas(t)">Hapus</button>
