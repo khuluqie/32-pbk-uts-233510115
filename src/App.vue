a606e7021c92bbbcb7cc7c6b80211bb1e0b2ab31
khuluqie <andikakhuluqi@student.uir.ac.id>
2025-05-02 13:54:07 +0700

    menampilkan tugas

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,12 @@ const tambahTugas = () => {
 <template>
   <input type="text" v-model="tugasBaru" @keyup.enter="tambahTugas">
   <button @click="tambahTugas">Tambah Tugas</button>
+
+  <ul>
+    <li v-for="t in tugas" :key="tugas.id">
+      {{ t.nama }}
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
